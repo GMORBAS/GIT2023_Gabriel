3ae19505ac4228f5a5924cee74e037c5a73d3f35
GMORBAS <115471585+GMORBAS@users.noreply.github.com>
2023-02-03 13:43:36 +0100

    Add files via upload

---
+++ Vehiculo.java
@@ -0,0 +1,22 @@
+
+public class Vehiculo {
+    
+    private String marca;
+    private String modelo;
+    private String matricula;
+    
+    public Vehiculo(String marca, String modelo, String matricula) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.matricula = matricula;
+    }
+    
+    public void mostrarMarca() {
+        System.out.println(marca);
+    }
+    
+    public void mostrarModelo() {
+        System.out.println(modelo);
+    }
+    
+}
